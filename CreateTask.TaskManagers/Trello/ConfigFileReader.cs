394622f8fd09fa912f1618c81361465818a439b4
Niklas Nihlen <niklas.nihlen@gmail.com>
2012-12-14 08:28:34 +0100

    Added to the TrelloTaskManager so that it asks for values that does not exist in the TrelloConfig.xml file

--- CreateTask.TaskManagers/Trello/ConfigFileReader.cs
+++ CreateTask.TaskManagers/Trello/ConfigFileReader.cs
@@ -7,6 +7,9 @@ namespace CreateTask.TaskManagers.Trello
   {
     private readonly XDocument confDoc;
     private readonly string configFile;
+    private readonly string key_idList = "idList";
+    private readonly string key_key = "key";
+    private readonly string key_token = "token";
 
     public ConfigFileReader(string configFile) {
       if (File.Exists(configFile + ".user")) {
@@ -15,19 +18,49 @@ namespace CreateTask.TaskManagers.Trello
       else {
         this.configFile = configFile;
       }
+
+      if (File.Exists(this.configFile) == false) {
+        var newFile = new XDocument(new XDeclaration("1.0", "utf-8", ""),
+                                    new XElement("TrelloConfig",
+                                                 new XElement(key_key, ""),
+                                                 new XElement(key_token, ""),
+                                                 new XElement(key_idList, "")
+                                      )
+          );
+        newFile.Save(this.configFile);
+      }
       confDoc = XDocument.Load(this.configFile);
     }
 
     public string IdList {
-      get { return confDoc.Root.Element("idList").Value; }
+      get {
+        EnsureElementValueExists(key_idList);
+        return confDoc.Root.Element(key_idList).Value;
+      }
     }
 
     public string Key {
-      get { return confDoc.Root.Element("key").Value; }
+      get {
+        EnsureElementValueExists(key_key);
+        return confDoc.Root.Element(key_key).Value;
+      }
     }
 
     public string Token {
-      get { return confDoc.Root.Element("token").Value; }
+      get {
+        return confDoc.Root.Element(key_token).Value;
+      }
+    }
+
+    private void EnsureElementValueExists(string elementName) {
+      if (string.IsNullOrEmpty(confDoc.Root.Element(elementName).Value)) {
+        string newKey = string.Empty;
+        InputBox.Show(elementName + " missing",
+                      string.Format("Hi there, we're missing your {0} for Trello. Please enter it below.", elementName), 
+                      ref newKey);
+        confDoc.Root.Element(elementName).Value = newKey;
+        confDoc.Save(configFile);
+      }
     }
 
     public void WriteToken(string newToken) {
